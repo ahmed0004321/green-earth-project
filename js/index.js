b99e54b310e01342fa2d1d173758321ea0a76adf
ahmed0004321 <oasifrikto@gmail.com>
2025-09-09 00:21:30 +0600

    added the total, count and modal functionallity

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 //all container stored here//
 const categoryContainer = document.getElementById("category-container");
 const treeCardContainer = document.getElementById("tree-card-container");
-const cartContainer = document.getElementById('cart-container')
+const cartContainer = document.getElementById('cart-container');
+const detailsContainer = document.getElementById('details-container');
 //tree category load
 //load category is done
 const loadTreeCategories = async () => {
@@ -15,7 +16,7 @@ const loadTreeCategories = async () => {
   category.forEach((cat) => {
     const li = document.createElement("li");
     li.innerHTML = `
-        <li onclick="plantByCategories('${cat.id}')" class="hover:bg-[#3b813e] font-semibold p-2 hover:text-white rounded-xl">${cat.category_name}</li>
+        <li onclick="plantByCategories('${cat.id}')" class="hover:bg-[#3b813e] font-semibold p-2 hover:text-white rounded-xl border-b-1 border-gray-300">${cat.category_name}</li>
         `;
     categoryContainer.appendChild(li);
   });
@@ -48,7 +49,7 @@ const allPlants = async () => {
   treeCardContainer.innerHTML = "";
 
   plant.forEach((plant) => {
-    //console.log(plant.image);
+    
     const div = document.createElement("div");
     div.innerHTML = `
           <div id="cards" class="bg-white shadow-sm p-4 rounded-lg flex flex-col h-full w-full">
@@ -57,7 +58,7 @@ const allPlants = async () => {
     </div>
 
     <div class="flex flex-col flex-grow mt-3">
-        <h3 class="font-bold text-lg truncate">${plant.name}</h3>
+        <h3 onclick="loadTreeDetail('${plant.id}')" class="font-bold text-lg truncate name">${plant.name}</h3>
         <p class="text-sm text-gray-600 line-clamp-2">${plant.description}</p>
 
         <div class="mt-auto">
@@ -65,18 +66,41 @@ const allPlants = async () => {
                 <p class="bg-[#dcfce7] px-3 py-1 text-[12px] rounded-3xl">${plant.category}</p>
                 <p class="font-bold">৳${plant.price}</p>
             </div>
-            <button onclick="addToCart('${plant.name}')" class="btn bg-[#3b813e] text-white rounded-full w-full py-2">
+            <button onclick="addToCart('${plant.name}', ${plant.price})" class="btn bg-[#3b813e] text-white rounded-full w-full py-2">
                 Add To Cart
             </button>
         </div>
     </div>
 </div>
-
-        `;
+`;
     treeCardContainer.appendChild(div);
+
   });
 };
 allPlants();
+
+//tree detail load
+const loadTreeDetail = async (id) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/plant/${id}`);
+    const json = await response.json();
+    const data = json.plants;
+    showModal(data);
+}
+
+const showModal = (data) => {
+    detailsContainer.innerHTML = `
+    <div class="flex flex-col space-y-3">
+        <h3 class="font-bold">${data.name}</h3>
+        <img class="w-full h-full object-cover rounded-lg" src="${data.image}" alt="">
+        <p class="">Category:<span> ${data.category}</span></p>
+        <p class="">Price:৳ <span>${data.price}</span></p>
+        <p class="">Description:<span> ${data.description}</span></p>
+      </div>
+    `; 
+    document.getElementById('my_modal').showModal();
+}
+
+
 
 //get plants by categories
 const plantByCategories = async (id) => {
@@ -97,7 +121,7 @@ const plantByCategories = async (id) => {
     </div>
 
     <div class="flex flex-col flex-grow mt-3">
-        <h3 class="font-bold text-lg truncate">${plant.name}</h3>
+        <h3 onclick="loadTreeDetail('${plant.id}')" class="font-bold text-lg truncate">${plant.name}</h3>
         <p class="text-sm text-gray-600 line-clamp-2">${plant.description}</p>
 
         <div class="mt-auto">
@@ -105,7 +129,7 @@ const plantByCategories = async (id) => {
                 <p class="bg-[#dcfce7] px-3 py-1 text-[12px] rounded-3xl">${plant.category}</p>
                 <p class="font-bold">৳${plant.price}</p>
             </div>
-            <button onclick="addToCart('${plant.name}')" class="btn bg-[#3b813e] text-white rounded-full w-full py-2">
+            <button onclick="addToCart('${plant.name}', ${plant.price})" class="btn bg-[#3b813e] text-white rounded-full w-full py-2">
                 Add To Cart
             </button>
         </div>
@@ -118,17 +142,31 @@ const plantByCategories = async (id) => {
 
 
 //add to cart
-const addToCart = (name) => {
-    
+const addToCart = (name, price) => {
+
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="rounded-2xl p-4 bg-[#cff0dc] mb-2 flex justify-between items-center">
+    <div class="space-y-1">
     <p class="text-[12px] font-semibold">${name}</p>
+    <p class="text-[#889396]">৳${price} x <span id="count">1</span></p>
+    </div>
     <i class="fa-solid fa-xmark"></i>
     </div>
     `
     cartContainer.appendChild(div);
+
+    //total 
+    const total = parseInt(document.getElementById('total').innerText);
+    const treePrice = parseInt(`${price}`);
+    
+    const totalPrice = total + treePrice;
+    document.getElementById('total').innerText = totalPrice;
+
+    
 }
+
+
 
 //delete cart item
 cartContainer.addEventListener('click', (e) =>{
